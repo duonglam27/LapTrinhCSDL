181a057874993af031f7a7ceb2cf206efd89d3a0
LamDuong27 <lamdnb27@gmail.com>
2025-03-16 04:31:55 +0700

    16/3

---
+++ WindowsFormsApp1/WindowsFormsApp1/FormLogin.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btDangKi = new System.Windows.Forms.Button();
+            this.btDangNhap = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbMatKhau = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbchuacotaikhoan = new System.Windows.Forms.Label();
+            this.lbDangNhap = new System.Windows.Forms.Label();
+            this.cbMatkhau = new System.Windows.Forms.CheckBox();
+            this.lbTaiKhoan = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbTen = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.cbMatkhau);
+            this.panel1.Controls.Add(this.btDangKi);
+            this.panel1.Controls.Add(this.lbDangNhap);
+            this.panel1.Controls.Add(this.btDangNhap);
+            this.panel1.Controls.Add(this.lbchuacotaikhoan);
+            this.panel1.Controls.Add(this.lbTaiKhoan);
+            this.panel1.Controls.Add(this.lbMatKhau);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 653);
+            this.panel1.TabIndex = 3;
+            // 
+            // btDangKi
+            // 
+            this.btDangKi.Location = new System.Drawing.Point(37, 566);
+            this.btDangKi.Name = "btDangKi";
+            this.btDangKi.Size = new System.Drawing.Size(353, 28);
+            this.btDangKi.TabIndex = 2;
+            this.btDangKi.Text = "Đăng Kí Ngay";
+            this.btDangKi.UseVisualStyleBackColor = true;
+            this.btDangKi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDangNhap.Location = new System.Drawing.Point(35, 420);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(355, 37);
+            this.btDangNhap.TabIndex = 2;
+            this.btDangNhap.Text = "Đăng Nhập";
+            this.btDangNhap.UseVisualStyleBackColor = true;
+            this.btDangNhap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(37, 336);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lbMatKhau
+            // 
+            this.lbMatKhau.AutoSize = true;
+            this.lbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatKhau.Location = new System.Drawing.Point(32, 308);
+            this.lbMatKhau.Name = "lbMatKhau";
+            this.lbMatKhau.Size = new System.Drawing.Size(97, 25);
+            this.lbMatKhau.TabIndex = 0;
+            this.lbMatKhau.Text = "Mật Khẩu";
+            this.lbMatKhau.Click += new System.EventHandler(this.lbMatKhau_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(35, 263);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbchuacotaikhoan
+            // 
+            this.lbchuacotaikhoan.AutoSize = true;
+            this.lbchuacotaikhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbchuacotaikhoan.Location = new System.Drawing.Point(138, 543);
+            this.lbchuacotaikhoan.Name = "lbchuacotaikhoan";
+            this.lbchuacotaikhoan.Size = new System.Drawing.Size(157, 20);
+            this.lbchuacotaikhoan.TabIndex = 0;
+            this.lbchuacotaikhoan.Text = "Chưa có tài khoản ?";
+            this.lbchuacotaikhoan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbDangNhap
+            // 
+            this.lbDangNhap.AutoSize = true;
+            this.lbDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDangNhap.Location = new System.Drawing.Point(32, 140);
+            this.lbDangNhap.Name = "lbDangNhap";
+            this.lbDangNhap.Size = new System.Drawing.Size(189, 32);
+            this.lbDangNhap.TabIndex = 0;
+            this.lbDangNhap.Text = "ĐĂNG NHẬP";
+            this.lbDangNhap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbMatkhau
+            // 
+            this.cbMatkhau.AutoSize = true;
+            this.cbMatkhau.Location = new System.Drawing.Point(37, 372);
+            this.cbMatkhau.Name = "cbMatkhau";
+            this.cbMatkhau.Size = new System.Drawing.Size(114, 20);
+            this.cbMatkhau.TabIndex = 3;
+            this.cbMatkhau.Text = "Hiện mật khẩu";
+            this.cbMatkhau.UseVisualStyleBackColor = true;
+            // 
+            // lbTaiKhoan
+            // 
+            this.lbTaiKhoan.AutoSize = true;
+            this.lbTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTaiKhoan.Location = new System.Drawing.Point(32, 235);
+            this.lbTaiKhoan.Name = "lbTaiKhoan";
+            this.lbTaiKhoan.Size = new System.Drawing.Size(103, 25);
+            this.lbTaiKhoan.TabIndex = 0;
+            this.lbTaiKhoan.Text = "Tài Khoản";
+            this.lbTaiKhoan.Click += new System.EventHandler(this.lbMatKhau_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.logoCoffeeShop;
+            this.pictureBox1.Location = new System.Drawing.Point(578, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbTen
+            // 
+            this.lbTen.AutoSize = true;
+            this.lbTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTen.Location = new System.Drawing.Point(419, 264);
+            this.lbTen.Name = "lbTen";
+            this.lbTen.Size = new System.Drawing.Size(437, 29);
+            this.lbTen.TabIndex = 0;
+            this.lbTen.Text = "COFFEE MANAGERMENT SYSTEM";
+            this.lbTen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(877, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbTen);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbDangNhap;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.Label lbchuacotaikhoan;
+        private System.Windows.Forms.Button btDangKi;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox cbMatkhau;
+        private System.Windows.Forms.Label lbTaiKhoan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbTen;
+    }
+}
